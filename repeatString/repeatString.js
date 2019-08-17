5315724418ec1145491009ce6bf343b8bc539e66
Anthony Bronkema <abronkema@customink.com>
2019-08-16 22:22:23 -0400

    completes repeatString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(phrase, repetitions) {
+  let phraseString = "";
+  if (repetitions < 0) {
+    return `ERROR`;
+  }
+  for (i = 1; i <= repetitions; i++) {
+    phraseString += phrase;
+  }
+  return phraseString;
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;

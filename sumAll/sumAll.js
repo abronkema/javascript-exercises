808be676a6674c77273755fc728e82cdc63d07b3
Anthony Bronkema <abronkema@customink.com>
2019-08-20 16:52:15 -0400

    completes sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startingInt, endingInt) {
+  let sum = 0;
+  if (typeof startingInt !== "number" || typeof endingInt !== "number") {
+    return "ERROR";
+  } else if (startingInt < 0 || endingInt < 0) {
+    return "ERROR";
+  } else if (startingInt > endingInt) {
+    [startingInt, endingInt] = [endingInt, startingInt];
+  }
+  for (i = startingInt; i <= endingInt; i++) {
+    sum += i;
+  }
+  return sum;
+};
 
-}
+console.log(sumAll(10, 1));
 
-module.exports = sumAll
+module.exports = sumAll;

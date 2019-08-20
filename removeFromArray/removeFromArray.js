a642e7ed05c09bb839e433eabb8e4333ab006fdb
Anthony Bronkema <abronkema@customink.com>
2019-08-20 09:03:35 -0400

    completes removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  // push
+  let removeArray = [...args];
+  let newArray = array.filter(function(element) {
+    return !removeArray.includes(element);
+  });
+  return newArray;
+};
 
-}
+let arrayOne = [1, 2, 3, "frank", "red"];
+console.log(removeFromArray(arrayOne, "frank", 3));
 
-module.exports = removeFromArray
+module.exports = removeFromArray;

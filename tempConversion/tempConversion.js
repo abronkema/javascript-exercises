4c43dde6f3aa13d415bd826006785b7a1fc0acc2
Anthony Bronkema <abronkema@customink.com>
2019-08-21 12:55:46 -0400

    completes tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,22 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  temp = (temp - 32) * eval(5 / 9);
+  if (temp.toFixed(1) == 0) {
+    return 0;
+  } else {
+    return parseFloat(temp.toFixed(1));
+  }
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function(temp) {
+  temp = temp * eval(9 / 5) + 32;
+  if (temp.toFixed(1) == 0) {
+    return 0;
+  } else {
+    return parseFloat(temp.toFixed(1));
+  }
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};

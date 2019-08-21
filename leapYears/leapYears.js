be1e789d4b4d20de4e770aa8bf79dace7a6d99ae
Anthony Bronkema <abronkema@customink.com>
2019-08-21 12:14:45 -0400

    completes leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  // leap year if year % 4 === 0
+  // leap year if year % 400 === 0
+  // not leap year if year % 100 === 0
+  if (year % 400 == 0) {
+    return true;
+  } else if (year % 4 == 0 && year % 100 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;

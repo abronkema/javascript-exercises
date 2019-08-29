fad55c35e74de00ae2d183e126e05c11dcc79f1c
Anthony Bronkema <abronkema@customink.com>
2019-08-29 15:58:35 -0400

    completes calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,38 @@
-function add () {
-	
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function sum () {
-	
+function sum(nums) {
+  return nums.reduce((a, b) => a + b, 0);
 }
 
-function multiply () {
-	
+function multiply(nums) {
+  return nums.reduce((a, b) => a * b);
 }
 
-function power() {
-	
+function power(num, pow) {
+  return Math.pow(num, pow);
 }
 
-function factorial() {
-	
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  for (let i = num - 1; i >= 1; i--) {
+    num = num * i;
+  }
+  return num;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};

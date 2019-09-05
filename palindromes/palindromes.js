0f55f0e5692868555a55d0377d068beb1de12cf7
Anthony Bronkema <abronkema@customink.com>
2019-09-05 13:42:45 -0400

    completes palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function() {
+const palindromes = function(string) {
+  let re = /[\W_]/gi;
+  const filtered = string
+    .split(re)
+    .join("")
+    .toLowerCase();
 
-}
+  const fwdArray = Array.from(filtered);
+  const bwdArray = Array.from(filtered).reverse();
 
-module.exports = palindromes
+  for (let i = 0; i < fwdArray.length; i++) {
+    if (fwdArray[i] !== bwdArray[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+module.exports = palindromes;
